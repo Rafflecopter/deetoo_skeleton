428609d4c6045831951e18164aa43eef9ecc8672
J.R. Patten <jr@juanpatten.com>
2012-07-16 16:26:41 -0600

    Initial skeleton

---
+++ my_service/service.js
@@ -0,0 +1,36 @@
+
+var DeeToo = require('./lib/deetoo')
+,   CONF = require('./config')
+
+,   d2 = new DeeToo(CONF)
+
+
+d2.can('hammer', function(job, $done) {
+
+  // A sample task that takes 10 seconds
+  // and updates progress 5 times
+
+  function nextStep() {
+    job.progress(++step, 5)
+    console.log('  ---]')
+  }
+
+  function stop() {
+    clearTimeout(ticker)
+    $done()
+    console.log('Finished hammering... ' + job.id)
+  }
+
+  var step = 0
+    , ticker = setInterval(nextStep, 2000)
+
+  setTimeout(stop, 10000)
+
+  console.log('Starting to hammer... ' + job.id)
+})
+
+d2.speaks('http')
+
+d2.start()
+
+
