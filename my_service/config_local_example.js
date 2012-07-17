e4c0bee11aef73896def4ad5e4942598f6b211eb
Justin Ratner <leroix08@gmail.com>
2012-07-16 18:05:12 -0600

    ocd changes

--- my_service/config_local_example.js
+++ my_service/config_local_example.js
@@ -3,7 +3,7 @@ module.exports = {
      PORT_WWW: 8008
 
     ,REDIS: {
-         host:   'someserver.com' 
+         host:  'someserver.com' 
         ,port:  24507
         ,user:  'GeorgeP'
         ,pass:  'Burdell'
